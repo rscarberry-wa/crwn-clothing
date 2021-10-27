578aaa799d7755f803d67b7b0ea4de538757ecd2
Randall Scarberry <drrandys@yahoo.com>
2021-10-27 09:48:06 -0400

    Stored authenticated users into firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config = {
     measurementId: "G-TFKSZ3VWLZ"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    // Will be null if called after sign out.
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        // Destructure out these fields
+        const { displayName, email } = userAuth;
+        // Time of creation
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName: displayName,
+                email: email,
+                createdAt: createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
