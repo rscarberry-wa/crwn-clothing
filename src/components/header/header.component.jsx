4057a37b1fe429bb5ebd7c563d59c826ede567f9
Randall Scarberry <drrandys@yahoo.com>
2021-10-29 14:45:11 -0400

    Implemented adding cart items keeping track of their quantities

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,9 +6,12 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import { connect } from 'react-redux';
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -23,11 +26,19 @@ const Header = ({ currentUser }) => (
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
         </div>
+        <CartIcon />
+        { hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-});
+// const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+//     currentUser,
+//     hidden
+// });
+
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser,
+    hidden: state.cart.hidden
+})
 
 export default connect(mapStateToProps)(Header);
